048147e55e4f7d572989bebb35dfb68a2360b787
Hins Chiu <chiuhi2@oocl.com>
2020-11-25 17:16:14 +0800

    [javaOOPractice][HinsChiu]implementation:practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,63 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+    private String displayName;
+    private Student leader;
+    private List<Student> memberList;
+    private Teacher teacher;
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.displayName = "Class " + number;
+        this.memberList = new ArrayList<>();
+    }
+
+    public Integer getNumber() { return number; }
+
+    public Student getLeader() { return leader; }
+
+    public String getDisplayName() { return displayName; }
+
+    public Teacher getTeacher() { return teacher; }
+
+    public void setTeacher(Teacher teacher) { this.teacher = teacher; }
+
+    public void appendMember(Student student) {
+        this.memberList.add(student);
+        if(teacher != null) {
+            System.out.print("I am " + teacher.getName() + ". I know " + student.getName() + " has joined " + this.displayName + ".\n");
+            if(isLeader(student)) {
+                System.out.print("I am " + teacher.getName() + ". I know " + student.getName() + " become Leader of " + this.displayName + ".\n");
+            }
+        }
+    }
+
+    public void assignLeader(Student leader) {
+        if(!isIn(leader)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+
+        if(teacher != null) {
+            System.out.print("I am " + teacher.getName() + ". I know " + leader.getName() + " become Leader of " + this.displayName + ".\n");
+        }
+
+        this.leader = leader;
+    }
+
+    public boolean isIn(Student student) {
+        return this.memberList.stream().anyMatch(member -> member.equals(student));
+    }
+
+    public boolean isLeader(Student student) {
+        if(this.leader == null) {
+            return false;
+        }
+
+        return this.leader.equals(student);
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,37 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> classes;
+
+    public Teacher(Integer id, String name, int age) {
+        super(id, name, age);
+        this.classes = new LinkedList<>();
+    }
+
+    public Teacher(Integer id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        for(Klass klass : classes) {
+            klass.setTeacher(this);
+        }
+    }
+
+    public List<Klass> getClasses() { return classes; }
+
+    @Override
+    public String introduce() {
+        return  super.introduce() + " I am a Teacher. " + (classes.size() == 0 ? "I teach No Class." : "I teach Class " + classes.stream().map(klass -> klass.getNumber().toString()).collect(Collectors.joining(", ")) + ".") ;
+    }
+
+    public String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. I" + (this.isTeaching(student) ? "" : " don't") + " teach " + student.getName() + ".";
+    }
+
+    public boolean isTeaching(Student student) {
+        return this.classes.stream().anyMatch(klass -> klass.getNumber().equals(student.getKlass().getNumber())) ;
+    }
 }
