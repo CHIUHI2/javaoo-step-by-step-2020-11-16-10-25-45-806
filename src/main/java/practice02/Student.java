f8f5cfbada3f986bfb198e4f5eed061821707481
Hins Chiu <chiuhi2@oocl.com>
2020-11-25 14:49:33 +0800

    [javaOOPractice][HinsChiu]implementation:practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,17 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private Integer klass;
+
+    public Student(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() { return klass; }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class " + klass + ".";
+    }
 }
